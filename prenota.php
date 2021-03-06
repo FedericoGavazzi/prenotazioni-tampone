0910f22aec89aa833a9ce10a3e0186d5ff4eeaae
FedericoGavazzi <federico.gavazzi02@gmail.com>
2021-03-06 12:26:16 +0100

    add insert data in db

--- prenota.php
+++ prenota.php
@@ -13,12 +13,18 @@ $charset = 'utf8';
 $dsn = 'mysql:host='.$host.';dbname='.$db;
 $pdo = new PDO($dsn, $user, $pass);
 
+//variabili da mettere nella query
 $codice_fiscale = 'BGTLSN00N00B157H';
 $data_scelta = '2021-03-06';
 
+//preset query di inserimento
 $sql = "INSERT INTO prenotazioni VALUES (null, :codice_fiscale, :giorno)";
 
+//invio query al db che la tiene in attesa
+$stmt = $pdo ->prepare($sql);
 
+//invio i dati concreti
+$stmt -> execute(['codice_fiscale' => $codice_fiscale, 'giorno' => $data_scelta]);
 
 
 
