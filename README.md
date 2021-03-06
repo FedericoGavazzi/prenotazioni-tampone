3d6e2f7438acee23d15a56dd239148fdaca334e2
FedericoGavazzi <federico.gavazzi02@gmail.com>
2021-03-06 12:18:31 +0100

    connection to db

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Prenotazioni
 ## Esempio didattico per gestire le prenotazioni dei tamponi
 
-Durante lìepidemia COVID
+Durante lìepidemia COVID, una delle esigenze 
 
 ### Funzionalità
 - Inserire una **prenotazione** con codice fiscale e data
+
--- prenota.php
+++ prenota.php
@@ -1,5 +1,22 @@
 <?php
+//Dice a livello dello script che gli errori verranno mostrati e che verranno loggati
 
+ini_set('display_errors',1);
+ini_set('log_errors',0);
+
+$host = 'localhost';
+$db = 'prenotazioni_tamponi';
+$user = 'root';
+$pass = '';
+$charset = 'utf8';
+
+$dsn = 'mysql:host='.$host.';dbname='.$db;
+$pdo = new PDO($dsn, $user, $pass);
+
+$codice_fiscale = 'BGTLSN00N00B157H';
+$data_scelta = '2021-03-06';
+
+$sql = "INSERT INTO prenotazioni VALUES (null, :codice_fiscale, :giorno)";
 
 
 
