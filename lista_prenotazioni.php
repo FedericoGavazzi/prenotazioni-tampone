319766b5ed03f22f94cf53e661b3bd5bbc8cbba3
FedericoGavazzi <federico.gavazzi02@gmail.com>
2021-03-12 15:49:31 +0100

    add lista_prenotazioni.php table in html

--- lista_prenotazioni.php
+++ lista_prenotazioni.php
@@ -12,7 +12,29 @@ $dsn = 'mysql:host='.$host.';dbname='.$db;
 $pdo = new PDO($dsn, $user, $pass);
 
 $sql = "SELECT * FROM prenotazioni";
+echo '<table>
+<tr>
+<td>id</td>
+<td>codice fiscale</td>
+<td>data</td>
+</tr>';
 
 $stmt  = $pdo -> query($sql);
+while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $id = $row['id'];
+    $cod = $row['codice_fiscale'];
+    $data = $row['giorno'];
+    echo "<tr>
+          <td>$id</td>
+          <td>$cod</td>
+          <td>$data</td>
+          </tr>";
+}
+echo '</table>';
 
-var_dump($stmt->fetchAll());
+
+
+echo "
+<table>
+    <td></td>
+</table>";
